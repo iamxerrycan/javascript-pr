5ad24f812d814495878789b6259f3d6c6ecdae26
rajshish333 <rajshish333@gmail.com>
2023-11-30 19:23:44 +0530

    prrctc

---
+++ Top30.js
@@ -0,0 +1,82 @@
+// reverse a string
+
+// const { log } = require("console")
+
+// let str = "saare jaha se achha"
+
+// const savedstr = str.split(" ").map((word)=>{
+//    return word.split("").reverse().join("")
+// })
+
+// let res = savedstr.join(' ')
+
+// console.log(res);======================================
+
+//how to check its object or not ?
+
+// const checkarr =(el)=>{
+//      return Array.isArray(el)
+// }
+
+// console.log(checkarr([]));
+// console.log(checkarr({}));===================================
+
+////howw to empty an array 
+
+// let arr = [1,2,3,4,5]
+
+// arr.length =0 ;
+// console.log(arr);===================================
+
+// how to check integer or not ?
+
+// var a = 12.1;
+
+// if(a%1 == 0){
+//   console.log("integer");
+// }
+// else{
+//   console.log("not!");
+// }
+
+
+// make a duplicate an array
+
+// const duplicateanArray =(arr)=>{
+//   return arr.concat(arr)
+// }
+
+// console.log(duplicateanArray([1,2,3]));======================
+
+//
+// const strr = "raj";
+
+// const reverc = (str)=>{
+//   return str.split("").reverse().join("")
+// }
+
+// console.log(reverc(strr));============================
+
+// reverse a num in javascript
+
+// let num = 3454;
+
+// function reverseNum(n) {
+//   var res = 0;
+//   while (n > 0) {
+//     var rem = n % 10;
+//     res = res * 10 + rem;
+//     n = Math.floor(n / 10);
+//   }
+//   return res;
+// }
+
+// console.log(reverseNum(12345));======================
+
+// write a string that return alphabetical string 
+
+// let alphabeticalStr =(str)=>{
+//   return str.split("").sort().join("")
+// }
+
+// console.log(alphabeticalStr("asdfgh"));=====================================
--- rough.js
+++ rough.js
@@ -1,52 +1,16 @@
-// import {fetch} from 'node-fetch'
-// let employ = [
-//   { name: "rahul", age: 24, id: 1 },
-//   { name: "rakesh", age: 25, id: 2 },
-//   { name: "rohit", age: 26, id: 3 },
-// ];
-// const data = []
-// employ.filter((s)=>{
-//     data.push(s.id)
-// })
+// // object occurence of alphabate corrector
 
-// console.log(data);
-// const dat = []
-// employ.map((i)=>{
-//     dat.push( i.age ,i.id , i.name)
-// })
+// let ocurrence = (srt) => {
+//   let occ = {};
+//   srt.split("").forEach((e) => {
+//     if (occ.hasOwnProperty(e) === false) {
+//       occ[e] = 1;
+//     } else {
+//       occ[e]++;
+//     }
+//   });
+//   return occ;
+// };
 
-// console.log(dat);
+// console.log(ocurrence("apple")); =========================
 
-// Fetching data from an API using async/await
-async function fetchData() {
-  try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    console.log(response, "response of api data============================");
-    if (!response.ok) {
-      throw new Error("Failed to fetch data");
-    }
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    return null;
-  }
-}
-
-async function storeData() {
-  try {
-    const fetchedData = await fetchData();
-
-    if (fetchedData) {
-      localStorage.setItem("storedData", JSON.stringify(fetchedData));
-      console.log("Data stored successfully:", fetchedData);
-    } else {
-      console.log("No data fetched or failed to fetch data.");
-    }
-  } catch (error) {
-    console.error("Error storing data:", error);
-  }
-}
-
-storeData();
